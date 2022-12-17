f9c3e51c948f1eefcb71d5c4b003b3080750efb9
Suven-p <pandeysuven@gmail.com>
2022-12-17 19:56:05 +0545

    Add header and footer to code prompt

--- backend/Chapters/Chapter.py
+++ backend/Chapters/Chapter.py
@@ -4,13 +4,25 @@ from bs4 import BeautifulSoup
 class Chapter:
     def __init__(self):
         self.prompt = ""
-        self.code = """
+        self.code = ""
+        self.header = """<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+        """
+        self.footer = """</body>
+        </html>
         """
 
     def get(self):
         return {
             "prompt": self.prompt,
-            "code": self.code.strip(),
+            "code": self.header + self.code.strip() + self.footer,
         }
 
     def verify(self, code: BeautifulSoup):
