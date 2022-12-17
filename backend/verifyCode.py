dd278e7db5b733da35b91e96609a34a39bcd23fa
Suven-p <pandeysuven@gmail.com>
2022-12-17 19:55:45 +0545

    Fix return format - Extra encapsulation

--- backend/verifyCode.py
+++ backend/verifyCode.py
@@ -4,8 +4,5 @@ from bs4 import BeautifulSoup
 
 def verifyCode(chapterId, code):
     chapter = ALL_CHAPTERS[chapterId]
-    soup = BeautifulSoup(code, "html5lib")
-    messages = chapter.verify(soup)
-    return {
-        "messages": messages,
-    }
+    soup = BeautifulSoup(code, "html.parser")
+    return chapter.verify(soup)
