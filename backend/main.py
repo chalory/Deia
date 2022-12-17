46e9e8f2de6d647b55e53c88546e6a0fbc53754b
Suven-p <pandeysuven@gmail.com>
2022-12-17 19:55:16 +0545

    Add CORS

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
 from verifyCode import verifyCode
 import Chapters
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route("/")
@@ -10,9 +12,12 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+@app.route("/chapters")
 @app.route("/chapters/<id>", methods=["GET", "POST"])
-def chapters(id):
+def chapters(id=None):
     all_chapters = Chapters.ALL_CHAPTERS
+    if id is None and request.method == "GET":
+        return jsonify({"chapters": list(all_chapters.keys())})
     if not id in all_chapters.keys():
         message = {"type": "error", "messages": "Invalid chapter id"}
         return jsonify(message), 400
